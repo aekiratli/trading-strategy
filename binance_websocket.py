cfdb47939e863263b40443947353408592df42f0
aekiratli <emre719@gmail.com>
2024-01-30 15:39:00 +0300

    pmax fix, split code into utils

--- binance_websocket.py
+++ binance_websocket.py
@@ -1,149 +1,21 @@
 from binance import AsyncClient, BinanceSocketManager
-from binance.client import Client
 import asyncio
 import pandas as pd
 import os
-import json
 import talib
 import logging
 import numpy as np
 import aiohttp
 from technical.indicators import PMAX
 import warnings
-
-warnings.filterwarnings('ignore')
-
-# library to .env file
+from utils import *
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
+warnings.filterwarnings('ignore')
 TELEGRAM_KEY = os.getenv("TELEGRAM_KEY")
 CHAT_ID = os.getenv("CHAT_ID")
-PARITIES_PATH = os.getenv("PARITIES_PATH")
-STATE_PATH = os.getenv("STATE_PATH")
-
-# Configure the logging module
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-client = Client("","")
-
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
-
-# Function to send a message to a Telegram user or group using aoidhttp
-async def telegram_bot_sendtext(msg):
-
-    bot_token = TELEGRAM_KEY
-    bot_chatID = CHAT_ID
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
-    async with aiohttp.ClientSession() as session:
-        async with session.get(send_text) as resp:
-            pass
-            #print(await resp.text())
-
-
-def update_state_file(file_name, state, state_value):
-    state_file_path = f'{STATE_PATH}/{file_name}_state.json'
-
-    try:
-        # Read the existing state file
-        with open(state_file_path, 'r') as state_file:
-            data = json.load(state_file)
-            
-        # Update or add the new state
-        data[state] = state_value
-
-        # Write the updated content back to the file
-        with open(state_file_path, 'w') as state_file:
-            json.dump(data, state_file, indent=2, cls=NpEncoder)
-
-
-    except FileNotFoundError:
-        # Handle the case where the file doesn't exist
-        print(f'Error: File {file_name}_state.json not found.')
-
-    except json.JSONDecodeError:
-        # Handle the case where the file is not a valid JSON
-        print(f'Error: {file_name}_state.json is not a valid JSON file.')
-
-        
-def initialize_state_files(file_names):
-    # create state directory if it doesn't exist
-    data = {"rsi": "n", "pmax": "n", "bbands": "n"}
-
-    if not os.path.exists(STATE_PATH):
-        os.makedirs(STATE_PATH)
-    # create state file if it doesn't exist
-    for file_name in file_names:
-        state_file_path = f'{STATE_PATH}/{file_name}_state.json'
-        if not os.path.exists(state_file_path):
-            with open(state_file_path, 'w') as state_file:
-                json.dump(data, state_file, indent=2)
-
-def read_state_file(file_name) -> dict:
-    state_file_path = f'{STATE_PATH}/{file_name}_state.json'
-    with open(state_file_path, 'r') as state_file:
-        state = json.load(state_file)
-        return state
-    
-def calculate_rsi_states(parity):
-    bounds = parity['lower_rsi_bounds'] + parity['upper_rsi_bounds']
-    bounds_keys = parity["rsi_states"]
-    bounds = sorted(bounds, reverse=True)
-    # print(bounds) [50, 45, 40, 35, 30]
-    # print(bounds_keys) ['h2', 'h1', 'n', 'l1', 'l2']
-    # create dict corresponding to the bounds with rsi_states
-    # e.g -> 51 is h2, 46 is h1, 41 is n, 36 is l1, 29 is l2
-    rsi_states = dict(zip(bounds_keys, bounds))
-    # add the last key to the dict using array index
-    rsi_states.update({bounds_keys[-1]: 0})
-    return rsi_states
-
-
-def calculate_rsi_state(rsi, rsi_states) -> str:
-    # example rsi_states -> {'h2': 50, 'h1': 45, 'n': 40, 'l1': 35, 'l2': 30, 'l3': 0}
-    # find the first key that is greater than the rsi
-    # e.g -> rsi = 51, rsi_state = h2
-    # e.g -> rsi = 46, rsi_state = h1
-    # e.g -> rsi = 41, rsi_state = n
-    # e.g -> rsi = 36, rsi_state = l1
-    # e.g -> rsi = 29, rsi_state = l2
-    # e.g -> rsi = 0, rsi_state = l3
-    for key, value in rsi_states.items():
-        if rsi >= value:
-            return key
-
-
-def initialize_parities() -> list:
-    # Get the list of files in the directory
-    files = os.listdir(PARITIES_PATH)
-
-    # Filter only JSON files
-    parities = []
-    file_names = []
-    json_files = [file for file in files if file.endswith('.json')]
-
-    # Import and process each JSON file
-    for json_file in json_files:
-        file_path = os.path.join(PARITIES_PATH, json_file)
-        # remove .json from file name
-        file_names.append(json_file[:-5])
-
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-            parities.append(data)
-    return parities, file_names
-
-def get_candles(symbol, interval, start) -> pd.DataFrame:
-    klines = client.get_historical_klines(symbol=symbol, interval=interval, start_str=start)
-    df = pd.DataFrame(klines, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
-    return df
 
 async def main(df, parity, task_id, file_name, state, rsi_states):
     is_first_run = True
@@ -200,6 +72,11 @@ async def main(df, parity, task_id, file_name, state, rsi_states):
                 calculate_pmax = True
                 if is_n_to_l_notif_sent == True:
                     pmax_candle_counter += 1
+                    if pmax_candle_counter == parity["pmax_candle_reset"] - 1:
+                        is_n_to_l_notif_sent = False
+                        pmax_candle_counter = 0
+                        logging.info(f"counter reset for symbol -> {parity['symbol']}, interval -> {parity['interval']}")
+
             else:
                 # candle is open, update last candle
                 df.loc[df.index[-1]] = [res['k']['t'], res['k']['o'], res['k']['h'], res['k']['l'], res['k']['c'], res['k']['v'], res['k']['T'], res['k']['q'], res['k']['n'], res['k']['V'], res['k']['Q'], res['k']['B']]
@@ -257,15 +134,8 @@ async def main(df, parity, task_id, file_name, state, rsi_states):
                             if pmax_candle_counter == 0:
                                 logging.info(f"pmax_prev_state -> {state['pmax']}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
                                 msg = f"🟪🟪🟪 *{parity['symbol']} - {parity['interval']}* - Price is on PMAX = {pmax:.2f} 🟪🟪🟪"
-                                state["pmax"] = pmax_state
                                 await telegram_bot_sendtext(msg)
                                 is_n_to_l_notif_sent = True
-                                update_state_file(file_name, 'pmax', pmax_state)
-
-                            elif pmax_candle_counter == parity["pmax_candle_reset"]:
-                                is_n_to_l_notif_sent = False
-                                pmax_candle_counter = 0
-                                logging.info(f"counter rested for -> {pmax_candle_counter}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
                             else:
                                 logging.info(f"counter -> {pmax_candle_counter}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
 
@@ -273,15 +143,8 @@ async def main(df, parity, task_id, file_name, state, rsi_states):
                             if pmax_candle_counter == 0:
                                 logging.info(f"pmax_prev_state -> {state['pmax']}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
                                 msg = f"🟪🟪🟪 *{parity['symbol']} - {parity['interval']}* - Price is on PMAX = {pmax:.2f} 🟪🟪🟪"
-                                state["pmax"] = pmax_state
                                 await telegram_bot_sendtext(msg)
                                 is_n_to_l_notif_sent = True
-                                update_state_file(file_name, 'pmax', pmax_state)
-
-                            elif pmax_candle_counter == parity["pmax_candle_reset"]:
-                                is_n_to_l_notif_sent = False
-                                pmax_candle_counter = 0
-                                logging.info(f"counter rested for -> {pmax_candle_counter}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
                             else:
                                 logging.info(f"counter -> {pmax_candle_counter}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
 
@@ -290,10 +153,10 @@ async def main(df, parity, task_id, file_name, state, rsi_states):
                                 logging.info(f"pmax_prev_state -> {state['pmax']}, pmax_state -> {pmax_state}, symbol -> {parity['symbol']}, interval -> {parity['interval']}, pmax -> {pmax}, ma -> {pmax_df.iloc[-1,-3]}, close -> {close}")
                                 msg = f"🟨🟨🟨 *{parity['symbol']} - {parity['interval']}* Price = {close:.2f} is close to PMAX = {pmax:.2f}  🟨🟨🟨"
                                 state["pmax_open_time"] = df.iloc[-1]['open_time']
-                                state["pmax"] = pmax_state
                                 update_state_file(file_name, 'pmax_open_time', df.iloc[-1]['open_time'])
                                 await telegram_bot_sendtext(msg)
-                                update_state_file(file_name, 'pmax', pmax_state)
+                        update_state_file(file_name, 'pmax', pmax_state)
+                        state["pmax"] = pmax_state
 
             # calculate bolinger bands
             if parity["bbands"] == True:
@@ -341,4 +204,3 @@ async def run_parities():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run_parities())
-
---
+++ utils.py
@@ -0,0 +1,133 @@
+import os
+from dotenv import load_dotenv
+import json
+import numpy as np
+import aiohttp
+from binance.client import Client
+import pandas as pd
+
+load_dotenv()
+client = Client("","")
+
+TELEGRAM_KEY = os.getenv("TELEGRAM_KEY")
+CHAT_ID = os.getenv("CHAT_ID")
+PARITIES_PATH = os.getenv("PARITIES_PATH")
+STATE_PATH = os.getenv("STATE_PATH")
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+# Function to send a message to a Telegram user or group using aoidhttp
+async def telegram_bot_sendtext(msg):
+
+    bot_token = TELEGRAM_KEY
+    bot_chatID = CHAT_ID
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
+    async with aiohttp.ClientSession() as session:
+        async with session.get(send_text) as resp:
+            pass
+
+def update_state_file(file_name, state, state_value):
+    state_file_path = f'{STATE_PATH}/{file_name}_state.json'
+
+    try:
+        # Read the existing state file
+        with open(state_file_path, 'r') as state_file:
+            data = json.load(state_file)
+            
+        # Update or add the new state
+        data[state] = state_value
+
+        # Write the updated content back to the file
+        with open(state_file_path, 'w') as state_file:
+            json.dump(data, state_file, indent=2, cls=NpEncoder)
+
+
+    except FileNotFoundError:
+        # Handle the case where the file doesn't exist
+        print(f'Error: File {file_name}_state.json not found.')
+
+    except json.JSONDecodeError:
+        # Handle the case where the file is not a valid JSON
+        print(f'Error: {file_name}_state.json is not a valid JSON file.')
+
+        
+def initialize_state_files(file_names):
+    # create state directory if it doesn't exist
+    data = {"rsi": "n", "pmax": "n", "bbands": "n"}
+
+    if not os.path.exists(STATE_PATH):
+        os.makedirs(STATE_PATH)
+    # create state file if it doesn't exist
+    for file_name in file_names:
+        state_file_path = f'{STATE_PATH}/{file_name}_state.json'
+        if not os.path.exists(state_file_path):
+            with open(state_file_path, 'w') as state_file:
+                json.dump(data, state_file, indent=2)
+
+def read_state_file(file_name) -> dict:
+    state_file_path = f'{STATE_PATH}/{file_name}_state.json'
+    with open(state_file_path, 'r') as state_file:
+        state = json.load(state_file)
+        return state
+    
+def calculate_rsi_states(parity):
+    bounds = parity['lower_rsi_bounds'] + parity['upper_rsi_bounds']
+    bounds_keys = parity["rsi_states"]
+    bounds = sorted(bounds, reverse=True)
+    # print(bounds) [50, 45, 40, 35, 30]
+    # print(bounds_keys) ['h2', 'h1', 'n', 'l1', 'l2']
+    # create dict corresponding to the bounds with rsi_states
+    # e.g -> 51 is h2, 46 is h1, 41 is n, 36 is l1, 29 is l2
+    rsi_states = dict(zip(bounds_keys, bounds))
+    # add the last key to the dict using array index
+    rsi_states.update({bounds_keys[-1]: 0})
+    return rsi_states
+
+
+def calculate_rsi_state(rsi, rsi_states) -> str:
+    # example rsi_states -> {'h2': 50, 'h1': 45, 'n': 40, 'l1': 35, 'l2': 30, 'l3': 0}
+    # find the first key that is greater than the rsi
+    # e.g -> rsi = 51, rsi_state = h2
+    # e.g -> rsi = 46, rsi_state = h1
+    # e.g -> rsi = 41, rsi_state = n
+    # e.g -> rsi = 36, rsi_state = l1
+    # e.g -> rsi = 29, rsi_state = l2
+    # e.g -> rsi = 0, rsi_state = l3
+    for key, value in rsi_states.items():
+        if rsi >= value:
+            return key
+
+
+def initialize_parities() -> list:
+    # Get the list of files in the directory
+    files = os.listdir(PARITIES_PATH)
+
+    # Filter only JSON files
+    parities = []
+    file_names = []
+    json_files = [file for file in files if file.endswith('.json')]
+
+    # Import and process each JSON file
+    for json_file in json_files:
+        file_path = os.path.join(PARITIES_PATH, json_file)
+        # remove .json from file name
+        file_names.append(json_file[:-5])
+
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            parities.append(data)
+    return parities, file_names
+
+
+def get_candles(symbol, interval, start) -> pd.DataFrame:
+    klines = client.get_historical_klines(symbol=symbol, interval=interval, start_str=start)
+    df = pd.DataFrame(klines, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+    return df
